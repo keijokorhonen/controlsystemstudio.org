afe7d1b26183c5d4b67f6e202e98dc28b44ca083
keijokorhonen <kkorhonenk.k@gmail.com>
2019-07-12 14:45:49 +0200

    Self-host fonts

---
+++ src/utils/typography.js
@@ -0,0 +1,11 @@
+import Typography from 'typography'
+
+const typography = new Typography({
+    baseFontSize: '18px',
+    baseLineHeight: 1.666,
+    headerFontFamily: ['Segoe UI', 'Roboto', 'Oxygen',
+        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', 'sans-serif'],
+    bodyFontFamily: ['Lato', 'Georgia', 'serif'],
+})
+
+export default typography
