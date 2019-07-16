7b67524ef148e247251f60721feeaf97a8a21120
keijokorhonen <kkorhonenk.k@gmail.com>
2019-07-16 16:19:57 +0200

    Add new OPI image

--- src/pages/features.js
+++ src/pages/features.js
@@ -19,6 +19,15 @@ const About = () => {
           }
         }
       }
+      opi: file(
+        relativePath: { eq: "CS-Studio-OPIs_and_Keyvisual_v03_big.png" }
+      ) {
+        childImageSharp {
+          fluid(maxWidth: 3840, maxHeight: 2160) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
       beast: file(relativePath: { eq: "beast.png" }) {
         childImageSharp {
           fluid(maxWidth: 487) {
@@ -39,8 +48,39 @@ const About = () => {
   return (
     <Layout>
       <SEO title="Features" />
-      <Banner imageFluid={images.banner.childImageSharp.fluid} text="Features" />
-      <Container>
+      <Banner
+        imageFluid={images.banner.childImageSharp.fluid}
+        text="Features"
+      />
+      <Container style={{ marginTop: `2rem` }} >
+        <h2>DisplayBuilder</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <ul>
+          <li>
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat.
+          </li>
+          <li>
+            Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur.
+          </li>
+          <li>
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum.
+          </li>
+          <li>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo.
+          </li>
+        </ul>
+        <Img fluid={images.opi.childImageSharp.fluid} 
+          style={{ maxWidth: `80%`, margin: `3rem 0 4rem` }}/>
+        <div style={{ marginBottom: `3rem` }}></div>
         <h2>BEAST - The Best Ever Alarm System Toolkit</h2>
         <p>
           This is the distributed alarm system developed at the SNS, consisting
@@ -83,7 +123,7 @@ const About = () => {
         <a href="https://github.com/ControlSystemStudio/cs-studio/wiki/BEAST">
           Read more
         </a>
-        <div style={{ marginBottom: `2rem` }}></div>
+        <div style={{ marginBottom: `3rem` }}></div>
         <h2>BOY - Best OPI, Yet</h2>
         <p>
           CSS BOY is an Operator Interface (OPI) development and runtime
@@ -119,7 +159,7 @@ const About = () => {
         <a href="https://github.com/ControlSystemStudio/cs-studio/wiki/BOY">
           Read more
         </a>
-        <div style={{ marginBottom: `2rem` }}></div>
+        <div style={{ marginBottom: `3rem` }}></div>
         <h2>DataBrowser</h2>
         <p>
           The Data Browser is a CSS tool for displaying live as well as
@@ -133,7 +173,7 @@ const About = () => {
         <p>
           The current plot can be emailed or sent to a log book or as email.
         </p>
-        <div style={{ marginBottom: `2rem` }}></div>
+        <div style={{ marginBottom: `3rem` }}></div>
         <h2>Logbook Support</h2>
         <p>
           The Data Browser, PACE and maybe other tools use org.csstudio.logbook
@@ -143,7 +183,7 @@ const About = () => {
           There are pluggable implementations for the SNS "ELog" and maybe
           others.
         </p>
-        <div style={{ marginBottom: `2rem` }}></div>
+        <div style={{ marginBottom: `3rem` }}></div>
         <h2>Scan Client</h2>
         <p>
           The scan system can automate experiments. For example, it can move a
